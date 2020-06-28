6e5ff4fead1ccf1b1031780c178f4d5ee6ac0a1e
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-06-29 00:13:15 +0300

    Изменили установку окружения в Linux Добавили красивый вывод этапов установку окуржения

--- set_env/install.sh
+++ set_env/install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-cd ..
-rm -fr venv   # На всякий случай удаляем предыдущее окружение
-python3 -m venv venv
-source ./venv/bin/activate
-pip install --upgrade pip
-pip install -r ./set_env/requirements.txt
-
+echo ------------------starting----------------------
+# На всякий случай удаляем предыдущее окружение
+rm -fr ../venv >> install.log
+echo making 'venv' directory
+python3 -m venv ../venv >> install.log
+echo upgrading pip
+../venv/bin/pip install --upgrade pip >> install.log 2>> erorr_install.log
+echo "pip installing requirements(python libraries)"
+../venv/bin/pip install -r ./requirements.txt >> install.log 2>> erorr_install.log
 # Чтобы изменения в файле config не было видно git и мы случайно его не закомментили в ветку
-git update-index --assume-unchanged config.py
+echo making 'config.py' file invisible to git
+git update-index --assume-unchanged ../config.py >> install.log
+
+echo deliting log files
+rm -f ./*.log
+echo ------------------complited----------------------
