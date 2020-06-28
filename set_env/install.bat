bef6546f26640724429474384cdafaf0fc3e0d6f
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-06-28 21:56:25 +0300

    Обновили установку окружения на Windows Теперь красивенько выводятся этапы установки без сообщений об ошибках и вывода команд

--- set_env/install.bat
+++ set_env/install.bat
@@ -1,7 +1,15 @@
-cd ..
-IF EXIST venv rd /s /q venv
-python -m venv venv
-venv\Scripts\pip install --upgrade pip
-venv\Scripts\pip install -r ./set_env/requirements.txt
-git update-index --assume-unchanged config.py
-cd set_env
+@echo off
+echo ------------------starting----------------------
+IF EXIST ..\venv rd /s /q ..\venv >> install.log
+echo making 'venv' directory
+python -m venv ..\venv >> install.log
+echo upgrading pip
+..\venv\Scripts\pip install --upgrade pip >> install.log 2>> erorr_install.log
+echo pip installing requirements(python libraries)
+..\venv\Scripts\pip install -r .\requirements.txt >> install.log 2>> erorr_install.log
+echo making 'config.py' file invisible to git
+git update-index --assume-unchanged ..\config.py  >> install.log
+
+echo deliting log files
+del /q .\*.log
+echo ------------------complited----------------------
