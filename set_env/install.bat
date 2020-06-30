227f198edaf40428f5d168dff3150489cccc5209
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-06-30 22:33:21 +0300

    Решили ошибку, возникающуу при обновлении pip

--- set_env/install.bat
+++ set_env/install.bat
@@ -4,7 +4,7 @@ IF EXIST ..\venv rd /s /q ..\venv >> install.log
 echo making 'venv' directory
 python -m venv ..\venv >> install.log
 echo upgrading pip
-..\venv\Scripts\pip install --upgrade pip >> install.log 2>> erorr_install.log
+..\venv\Scripts\pip install --upgrade pip --user >> install.log 2>> erorr_install.log
 echo pip installing requirements(python libraries)
 ..\venv\Scripts\pip install -r .\requirements.txt >> install.log 2>> erorr_install.log
 echo making 'config.py' file invisible to git
--- set_env/install.sh
+++ set_env/install.sh
@@ -5,7 +5,7 @@ rm -fr ../venv >> install.log
 echo making 'venv' directory
 python3 -m venv ../venv >> install.log
 echo upgrading pip
-../venv/bin/pip install --upgrade pip >> install.log 2>> erorr_install.log
+../venv/bin/pip install --upgrade pip --user >> install.log 2>> erorr_install.log
 echo "pip installing requirements(python libraries)"
 ../venv/bin/pip install -r ./requirements.txt >> install.log 2>> erorr_install.log
 # Чтобы изменения в файле config не было видно git и мы случайно его не закомментили в ветку
