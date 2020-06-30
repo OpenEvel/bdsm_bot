1c58ac313afe4406710e00d878ae37f9b69d9cf6
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-06-30 23:38:32 +0300

    Перенесли настроку окружения в manage.py Раньше всё выполнялось в установочных скриптов sh или bat

--- manage.py
+++ manage.py
@@ -1,66 +1,104 @@
 # -*- coding: utf-8 -*-
-import sys
 import os
-import telebot
-from telebot.types import Message
+import sys
+import time
+import subprocess
 
-import sqlite3
-from utils.admins.dber import AdminWorker
+def is_win():
+    return 'win' in sys.platform
 
-config_template = """# -*- coding: utf-8 -*-
-from enum import Enum
+ENCODING_CONSOLE = 'cp1251' if is_win() else 'utf8'
 
-TOKEN = '{TOKEN}'
-ADMIN_ID = {ADMIN_ID}
-DB_WORK = 'bases/work.db'  # Файл с базой данных о пользователях
-DB_STATES = 'bases/states.vdb'  # Файл с базой данных о состояних
+def status_bar(title:str=None, *, command:str, ts:float=0.25):
+    """
+        title   : сообщение пользователю (что будет выводиться вместо самой комманды)
+        command : комманда оболочки (что выполняется)
+        ts      : интервал времени между анимацией статус бара
+    """
+    if not title:
+        title = command
 
-class States(Enum):
-    START_ENTER = '0'
-    ENTER_PASSWOD = '1'
-    ENTER_COMPANY = '2'
-    AUTHORIZATED = '3'
-"""
+    # На всякий случай очищаем title от лишних пробелов(для красоты форматирования)
+    title = title.strip()
+
+    write, flush = sys.stdout.write, sys.stdout.flush
+    animations = ['|', '/', '-', '\\' ]  # Элементы анимации статус бара
+
+    # Запускаем комманду command через subprocess
+    process = subprocess.Popen(command,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    i_a = -1
+    while True:
+        if process.poll() is not None:
+            # При работе программы были ошибки
+            if process.poll() != 0:                
+                if process.stderr:
+                    write(f'\r\t{title}: [NO]\n')
+                    write(f'command: {command}\n')
+                    write('-----------------------ERROR MESSAGE---------------------\n')
+                    flush()
+                    while True:
+                        err = process.stderr.readline()
+                        err = err.decode(ENCODING_CONSOLE)
+
+                        if err == '':
+                            write('---------------------------------------------------------\n')
+                            flush()
+                            # Выходим из цикла вывода ошибок
+                            break
+                        if err != '':
+                            write(f'{err}')
+                            flush()
+            else:
+                # Прграмма завершилась нормально
+                write(f'\r\t{title}: [OK]\n')
+                flush()
+
+            # Выхлодим из бесконечного цикла анимации
+            break
+
+        # Получаем индекс анимации (не выходящий за пределы массива анимаций)
+        # По сути проходим по списку анимаций круг за кругом
+        i_a = (i_a + 1) % len(animations)
+
+        # Обновляем анимацию в статус баре (благодаря символу возврата каретки '\r')
+        write(f'\r\t{title}: [{animations[i_a]}]')
+        flush()
+
+        # Делаем перерыв между анимациями, чтобы не получилась каша
+        time.sleep(ts)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
 
-    if '--configure' in args:
-        if len(args) == 2:
-            args.remove('--configure')
-            TOKEN = args[0]
-            bot = telebot.TeleBot(TOKEN)
+    if '--install' in args:
+        if is_win():
+            status_bar("making 'venv' directory", command="python -m venv venv")
+            status_bar("upgrading pip", command='.\\venv\\Scripts\\pip install --upgrade pip --user')
+            status_bar("pip installing requirements(python libraries)", command=".\\venv\\Scripts\\pip install -r .\\set_env\\requirements.txt")
 
-            @bot.message_handler(content_types=["text"])
-            def repeat_all_messages(message: Message): # Название функции не играет никакой роли
-                ADMIN_ID = message.from_user.id
-                config_content = config_template.format(TOKEN=TOKEN, ADMIN_ID=ADMIN_ID)
-                f = open('config.py', 'w', encoding='utf-8')
-                f.write(config_content)
-                f.close()
-                bot.send_message(message.chat.id, 'Конфиг обновлён')
-                print('Конфиг обновлён')
+    if '--config' in args:
+        if 'visible' in args:
+            os.system("git update-index --no-assume-unchanged config.py")
+        else:
+            # Делаем config.py невидимым для git
+            os.system("git update-index --assume-unchanged config.py")
 
-                import config
-                if os.path.exists(config.DB_WORK):
-                    os.remove(config.DB_WORK)
-                if os.path.exists(config.DB_STATES):
-                    os.remove(config.DB_STATES)
-
-                conn = sqlite3.connect(config.DB_WORK)
-                f_sql = open(config.DB_WORK[:-2] + 'sql', encoding='utf-8')
-                conn.executescript(f_sql.read())
-                f_sql.close()
-                conn.close()
-
-                adminer = AdminWorker()
-                adminer.add(message.from_user)
-                adminer.close()
-
-                bot.send_message(message.chat.id, 'Вы добавлены в базу как админ')
-                print('Вы добавлены в базу как админ')
-
-
-            bot_inf = bot.get_me() 
-            print(f'Отправь мне что-нибудь https://t.me/{bot_inf.username}')
-            bot.infinity_polling()
+            # Получаем токен от пользователя
+            args.remove('--config')
+            if '--install' in args:
+                args.remove("--install")
+            if args:
+                TOKEN = args[0]
+            else:
+                TOKEN = input("Введите токен: ")
+            # В строке токина могут содержаться лишние символы
+            TOKEN = TOKEN.strip()
+            # Кто-то по ошибке мог вставить в строку токин кавычки
+            trash = ["\'", "\""]
+            for bad in trash:
+                TOKEN = TOKEN.replace(bad, '')
+            
+            # Устанавливаем файл настроек для работы бота
+            if is_win():
+                os.system(f'.\\venv\\Scripts\\python .\\set_env\\install_config.py {TOKEN}')
