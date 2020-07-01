55a6bc57b4e04dd0c591345eb7c134acc137722a
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-07-01 14:17:13 +0300

    Изменили установку файла конфига
    Теперь не используется шаблон, а используется исходный файл, что намного лучше, если в будущем будем обновлять его

--- set_env/install_config.py
+++ set_env/install_config.py
@@ -11,21 +11,6 @@ sys.path.append(workspace_folder)
 import sqlite3
 from utils.admins.dber import AdminWorker
 
-config_template = """# -*- coding: utf-8 -*-
-from enum import Enum
-
-TOKEN = '{TOKEN}'
-ADMIN_ID = {ADMIN_ID}
-DB_WORK = 'bases/work.db'  # Файл с базой данных о пользователях
-DB_STATES = 'bases/states.vdb'  # Файл с базой данных о состояних
-
-class States(Enum):
-    START_ENTER = '0'
-    ENTER_PASSWOD = '1'
-    ENTER_COMPANY = '2'
-    AUTHORIZATED = '3'
-"""
-
 if __name__ == "__main__":
     args = sys.argv[1:]
     TOKEN = args[0]
@@ -35,10 +20,20 @@ if __name__ == "__main__":
     @bot.message_handler(content_types=["text"])
     def repeat_all_messages(message): # Название функции не играет никакой роли
         ADMIN_ID = message.from_user.id
-        config_content = config_template.format(TOKEN=TOKEN, ADMIN_ID=ADMIN_ID)
-        f = open(os.path.join(workspace_folder, 'config.py'), 'w', encoding='utf-8')
-        f.write(config_content)
-        f.close()
+        # config_content = config_template.format(TOKEN=TOKEN, ADMIN_ID=ADMIN_ID)
+        f_r = open(os.path.join(workspace_folder, 'config.py'), 'r', encoding='utf-8')
+        config_content = ""
+        for line in f_r:
+            if line.startswith("TOKEN ="):
+                line = f"TOKEN = '{TOKEN}'\n"
+            if line.startswith("ADMIN_ID ="):
+                line = f"ADMIN_ID = {ADMIN_ID}\n"
+            config_content += line
+        f_r.close()
+
+        f_w = open(os.path.join(workspace_folder, 'config.py'), 'w', encoding='utf-8')
+        f_w.write(config_content)
+        f_w.close()
         bot.send_message(message.chat.id, 'Конфиг обновлён')
         print('Конфиг обновлён')
 
