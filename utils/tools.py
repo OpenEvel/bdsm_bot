f57b765b5a274b2fe9b4c2fcb81ffe6e02b98329
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-07-02 14:48:58 +0300

    Добавили функцию full_path возвращает полный путь относительно директроии, в которой находится модуль

--- utils/tools.py
+++ utils/tools.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from importlib import import_module
 
 def load_handlers(module_name, module_bot='bot'):
@@ -15,6 +16,11 @@ def load_handlers(module_name, module_bot='bot'):
         raise AttributeError(f"Переменной бота '{module_bot}' НЕТ в модуле {module_name}")
     return bot
 
-
-def get_module_dir(str_file):
-    return str_file
+def full_path(file_var, relative_path):
+    """
+    file_var: переменная __file__ модуля, в которм используется эта функция
+    relative_path: относительный путь относительно папки модуля
+    возвращает полный путь относительно директроии, в которой находится модуль
+    """
+    module_dir = os.path.split(os.path.abspath(file_var))[0]
+    return os.path.normpath(os.path.join(module_dir, relative_path))
