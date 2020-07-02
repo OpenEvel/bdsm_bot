c93a5fb987ecc3f1e9bd75dd8dcd61d5ebb3b0fb
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-07-02 16:07:22 +0300

    Сделали config.py более красивым для глазу

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ def full_path(file_var, relative_path):
 
 TOKEN = 'YOUR_TOKEN'
 ADMIN_ID = 'YUOR_ID' # Должно быть число а не строка
-DB_WORK = full_path(__file__, 'bases/work.db')  # Файл с базой данных о пользователях
+DB_WORK = full_path(__file__, 'bases/work.db')       # Файл с базой данных о пользователях
 DB_STATES = full_path(__file__, 'bases/states.vdb')  # Файл с базой данных о состояних
 
 class States(Enum):
-    START_ENTER = '0'
+    START_ENTER   = '0'
     ENTER_PASSWOD = '1'
     ENTER_COMPANY = '2'
-    AUTHORIZATED = '3'
+    AUTHORIZATED  = '3'
