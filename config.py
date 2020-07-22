fef4102d532f77910e56368f2a4812697239cb88
ogurechik <37779327+ogurechik@users.noreply.github.com>
2020-07-23 00:57:05 +0300

    Убрали путь к базе данных состояний
    Теперь в sqlite просто есть таблица с состояниями пользователей, поэтому необходимость в отдельном файле базы данных как в случае vedis отпала

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ from tools import full_path
 TOKEN = 'YOUR_TOKEN'
 ADMIN_ID = 'YUOR_ID' # Должно быть число а не строка
 DB_WORK = full_path(__file__, 'bases/work.db')       # Файл с базой данных о пользователях
-DB_STATES = full_path(__file__, 'bases/states.vdb')  # Файл с базой данных о состояних
